5c36d4936c3ebdc54e7262d4e6def9f1bdb90fde
aditya <aditya.das2797@gmail.com>
2021-03-19 02:57:29 -0400

    session working

--- main.go
+++ main.go
@@ -1,29 +1,55 @@
 package main
 
 import (
+        "os"
+        "os/signal"
+        "syscall"
         "fmt"
-        //"github.com/bwmarrin/discordgo"
+        "flag"
+        "github.com/bwmarrin/discordgo"
     )
-
 
 var (
     Token string
-    Link  string
 )
 
-//func init() {
-//    flag.StringVar(&Token, "!", "", "Bot Token")
-//    flag.Link(&Link, ";", "", "Link")
-//}
+func init() {
+    flag.StringVar(&Token, "t", "", "Bot Token")
+    //flag.StringVar(&Link, "l", "", "Link")
+    flag.Parse()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate){
+    if m.Content[0:5] == "!link" {
+        URL := m.Content[5:]
+        shortened, err := shorten(URL)
+        if err != nil {
+            fmt.Println ("Error generating URl,", err)
+        }
+    s.ChannelMessageSend(m.ChannelID, shortened)
+    }
+}
 
 
 func main() {
-    test := "www.google.com"
-    shortened := shorten(test)
-    fmt.Println(shortened)
-    //dg, err := discordgo.New("Bot " + Token)
-    //if err != nil{
-    //    fmt.Println("Error creating Discord session,", err)
-    //    return
-    //}
+    dg, err := discordgo.New("Bot " + Token)
+    if err != nil{
+        fmt.Println("Error creating Discord session,", err)
+        return
+    }
+
+    dg.AddHandler(messageCreate)
+    dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+    err = dg.Open()
+
+    if err != nil {
+        fmt.Println("Error opening connection,",err)
+        return
+    }
+
+    sc := make(chan os.Signal, 1)
+    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
+    dg.Close()
 }
--- urlhandler.go
+++ urlhandler.go
@@ -3,18 +3,27 @@ package main
 import (
         "net/http"
         "io/ioutil"
+        "fmt"
     )
 
 
 
 
-func shorten(URL string) string {
+func shorten(URL string) (string, error) {
 
     API := "https://tinyurl.com/api-create.php?url="
     constructURL := API + URL
-    resp, _ := http.Get(constructURL)
-    bytes, _ := ioutil.ReadAll(resp.Body)
+    resp, errGet := http.Get(constructURL)
+    if errGet != nil{
+        fmt.Println("Error in completing http request.")
+        return "-1", errGet
+    }
+    bytes, errBytes := ioutil.ReadAll(resp.Body)
+    if errBytes != nil{
+        fmt.Println("Error in reading bytes")
+        return "-1", errGet
+    }
 
-    return string(bytes)
+    return string(bytes), nil
 }
 
